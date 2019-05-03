d8c416789552831856454bc15390d623746ba973
Artyom <a.vecherov@gmail.com>
2019-05-03 14:55:13 +0300

    first commit

---
+++ basics/numbers.js
@@ -0,0 +1,5 @@
+let studentScore = 11;
+let maxScore = 20;
+let percent = (studentScore / maxScore * 100).toFixed();
+
+console.log(percent + '%');
---
+++ basics/srtings.js
@@ -0,0 +1,9 @@
+let name = 'Artyom';
+
+console.log(name);
+
+let city, country, location;
+city = 'Minsk';
+country = 'Belarus'
+location = city + ', ' + country;
+console.log(location);
